03b5ed8527dd6aef96e85706c5392e6f192293cd
peregrino000 <63953615+peregrino000@users.noreply.github.com>
2020-04-19 10:14:23 -0500

    Create codigo.php
    acabo de pegar un codigo php

---
+++ codigo.php
@@ -0,0 +1,13 @@
+<?
+#EjemploDe.COM
+#Ejemplo de Bucle While en PHP
+
+$i = 0; //Inializamos la variable en 0
+while($i==10) {
+$i++; //Aumentamos $i en uno
+echo "i ahora equivale a ".$i."</br> "; //Mostramos texto
+}
+echo "Fin del bucle while. El bucle while terminó en ".$i."";
+?>
+
+Fuente: https://www.ejemplode.com/20-php/126-ejemplo_de_bucle_while_en_php.html#ixzz6K4VrxXWl
